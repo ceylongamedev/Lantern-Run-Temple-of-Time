a2222d6b9b5346b3d99deedd298433ad9207d9f3
malaka96 <madhubhashana655@gmail.com>
2026-01-23 22:15:46 +0530

    converted platform to web

--- Assets/Scripts/Enviroment/moving_tiles/RunnerTile.cs
+++ Assets/Scripts/Enviroment/moving_tiles/RunnerTile.cs
@@ -9,7 +9,6 @@ public class RunnerTile : MonoBehaviour
     {
         MeshRenderer mr = GetComponentInChildren<MeshRenderer>();
         //BoxCollider collider = GetComponent<BoxCollider>();
-        //Debug.Log("Length from collider : "+collider.bounds.size.z);
         //Length = collider.bounds.size.z;
         Debug.Log(Length);
     }
